659f975244a285155cdefc22c0210bfe4a77c1f1
oriel-beck <orielbeck@gmail.com>
2021-09-19 19:44:46 +0300

    oops

--- src/functions/getPlayers.ts
+++ src/functions/getPlayers.ts
@@ -5,10 +5,10 @@ import { ScoreSaberWrapperError, SmallPlayer } from '../structures'
 export async function getPlayers(input: string | number = 1): Promise<SmallPlayer[]> {
     let req
     if (typeof input === 'string') {
-        req = await petitio(`https://new.scoresaber.com/api/players/by-name/${input}`, 'GET').json<smallplayerinfo[] | apiError>()
+        req = await petitio(`https://new.scoresaber.com/api/players/by-name/${input}`, 'GET').json<{players: smallplayerinfo[]} | apiError>()
     } else if (Number.isSafeInteger(input)) {
-        req = await petitio(`https://new.scoresaber.com/api/players/${input}`, 'GET').json<smallplayerinfo[] | apiError>()
+        req = await petitio(`https://new.scoresaber.com/api/players/${input}`, 'GET').json<{players: smallplayerinfo[]} | apiError>()
     } else throw new ScoreSaberWrapperError('[PARAMETERS] : input has to be type of string, number or undefined!')
     if ('error' in req) throw new ScoreSaberWrapperError(`[SCORESABER] : ${req.error.message}`)
-    return req.map(p => new SmallPlayer(p))
+    return req.players.map(p => new SmallPlayer(p))
 }
