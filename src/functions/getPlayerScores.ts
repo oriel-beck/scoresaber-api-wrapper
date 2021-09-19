0e80f00f6413fc21f2f08778ea9394049aaf5832
oriel-beck <orielbeck@gmail.com>
2021-09-19 19:50:42 +0300

    oops again + bump version

--- src/functions/getPlayerScores.ts
+++ src/functions/getPlayerScores.ts
@@ -5,7 +5,7 @@ import { ScoreSaberScore, ScoreSaberWrapperError } from '../structures';
 export async function getPlayerScores(playerId: string, type: 'recent' | 'top', offset: number = 1): Promise<ScoreSaberScore[]> {
     if (!['recent', 'top'].includes(type)) throw new ScoreSaberWrapperError('[PARAMETERS] : type has to be recent or top!')
     if (!Number.isSafeInteger(offset)) throw new ScoreSaberWrapperError('[PARAMETERS] : offset is not a safe integer!')
-    const req = await petitio(`https://new.scoresaber.com/api/player/${playerId}/scores/${type}/${offset}`, 'GET').json<score[] | apiError>()
+    const req = await petitio(`https://new.scoresaber.com/api/player/${playerId}/scores/${type}/${offset}`, 'GET').json<{scores: score[]} | apiError>()
     if ('error' in req) throw new ScoreSaberWrapperError(`[SCORESABER] : ${req.error.message}`)
-    return req.map(s => new ScoreSaberScore(s))
+    return req.scores.map(s => new ScoreSaberScore(s))
 }
